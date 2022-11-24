6eba53f3ca86d7ab4f5ea773fcea4bbee33d2345
Sineeth <mandasineeth123@gmail.com>
2022-11-24 19:45:04 +0530

    Change in DB

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,19 +78,19 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
 
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'django_webapp',
-        'USER':'aiopsqa',
-        'PASSWORD':'aiopsqa',
-        'HOST':'18.210.129.161',
-        'PORT':'5432',
-    }
-
     # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME':'django_webapp',
+    #     'USER':'aiopsqa',
+    #     'PASSWORD':'aiopsqa',
+    #     'HOST':'18.210.129.161',
+    #     'PORT':'5432',
     # }
+
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
